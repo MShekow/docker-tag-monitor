033d8afb1933651f928204ecb47c3d94f7cfff8e
MShekow <s2mashek@uni-bonn.de>
2024-09-03 17:31:26 +0200

    Initial commit (dummy Reflex project)

---
+++ docker_tag_monitor/docker_tag_monitor.py
@@ -0,0 +1,68 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+import requests
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+    ...
+
+
+# def index() -> rx.Component:
+#     # Welcome Page (Index)
+#     return rx.container(
+#         rx.color_mode.button(position="top-right"),
+#         rx.vstack(
+#             rx.heading("Welcome to Reflex!", size="9"),
+#             rx.text(
+#                 "Get started by editing ",
+#                 rx.code(f"{config.app_name}/{config.app_name}.py"),
+#                 size="5",
+#             ),
+#             rx.link(
+#                 rx.button("Check out our docs!"),
+#                 href="https://reflex.dev/docs/getting-started/introduction/",
+#                 is_external=True,
+#             ),
+#             spacing="5",
+#             justify="center",
+#             min_height="85vh",
+#         ),
+#         rx.logo(),
+#     )
+
+class GithubState(rx.State):
+    url: str = "https://github.com/reflex-dev"
+    profile_image: str = (
+        "https://avatars.githubusercontent.com/u/104714959"
+    )
+
+    def set_profile(self, username: str):
+        if username == "":
+            return
+        github_data = requests.get(
+            f"https://api.github.com/users/{username}"
+        ).json()
+        self.url = github_data["url"]
+        self.profile_image = github_data["avatar_url"]
+
+
+def index():
+    return rx.hstack(
+        rx.link(
+            rx.avatar(src=GithubState.profile_image),
+            href=GithubState.url,
+        ),
+        rx.input(
+            placeholder="Your Github username",
+            on_blur=GithubState.set_profile,
+        ),
+    )
+
+
+app = rx.App()
+app.add_page(index)
---
+++ rxconfig.py
@@ -0,0 +1,7 @@
+import reflex as rx
+
+# Note, you can override the values via env vars that are simply capitalized
+
+config = rx.Config(
+    app_name="docker_tag_monitor",
+)
